ec09cb86d2ddb10fdea1988bee5e0f1ba9c92094
Anthony Sychev <hello@dm211.com>
2020-10-30 13:27:59 +0100

    -opciones lang separados al de opciones generales -db

--- templates/admin_options.php
+++ templates/admin_options.php
@@ -11,6 +11,8 @@ $fn_p = $cl_m->parse("POST");
 $fn_r = $cl_m->parse("REQ");
 $g_action = (isset($fn_g['action'])) ? $fn_g['action'] : null;
 
+// ----------------------------------------------------------------------------------
+
 //select pag inicial
 $fn_q_sel_init_p = $db->FetchAll("
 	SELECT *
@@ -27,53 +29,6 @@ if($fn_q_sel_init_p) foreach($fn_q_sel_init_p as $slk => $slv)
 	$fn_xtemplate_parse['parse'][] = "{$fn_page_args['stage_id']}.sel_init_page";	
 }
 
-//tipo de header
-$fn_header_type = (isset($CONFIG['site']['initial_page_header_type'])) ? $CONFIG['site']['initial_page_header_type'] : 'small';
-
-$fn_xtemplate_parse['assign'][] = array(
-	"selected_{$fn_header_type}" => 'selected'
-);
-$fn_xtemplate_parse['parse'][] = '';
-
-//lang row
-$fn_lang = (isset($CONFIG['site']['lang']) && isJson($CONFIG['site']['lang'])) ? json_decode($CONFIG['site']['lang']) : false;
-
-if($fn_lang) foreach($fn_lang as $tpk)
-{
-	$fn_xtemplate_parse['assign'][] = array(
-		'lang' => $tpk,
-	);
-	$fn_xtemplate_parse['parse'][] = "{$fn_page_args['stage_id']}.lang_row";
-
-	//table traducciones rows
-	$fn_xtemplate_parse['assign'][] = array(
-		'lang' => $tpk,
-	);
-	$fn_xtemplate_parse['parse'][] = "{$fn_page_args['stage_id']}.lang_table_row";
-	
-	//options default lang
-	$fn_xtemplate_parse['assign'][] = array(
-		'lang' => $tpk,
-		'selected' => ($tpk == $CONFIG['site']['defaultLang']) ? 'selected' : '',
-	);
-	$fn_xtemplate_parse['parse'][] = "{$fn_page_args['stage_id']}.sel_def_lang";
-}
-
-$fn_lang_sel_q = $db->FetchAll("
-	SELECT c.*, ac.*
-	FROM `countries` c
-	LEFT JOIN `apps_countries` ac ON(ac.`country_code`=c.`code`)
-	ORDER BY `country_name` ASC
-");
-
-if($fn_lang_sel_q) foreach($fn_lang_sel_q as $slk => $slv)
-{
-	$fn_for_data = object_to_array($slv);
-	
-	$fn_xtemplate_parse['assign'][] = $fn_for_data;
-	$fn_xtemplate_parse['parse'][] = "{$fn_page_args['stage_id']}.lang_sel";
-}
-
 // ----------------------------------------------------------------------------------
 
 $fn_options_q = $db->FetchAll("
@@ -87,10 +42,7 @@ if($fn_options_q) foreach($fn_options_q as $opk => $opv)
 {
 	$fn_options_array[$opv->options_key] = $opv->options_value;
 	
-	if($opv->options_key == 'redsys_mode' && $opv->options_value == 1) $fn_options_array["selected_{$opv->options_value}"] = 'selected';
-	if($opv->options_key == 'home_show_testimonios') $fn_options_array["home_show_testimonios_selected_{$opv->options_value}"] = 'selected';
 	if($opv->options_key == 'langSelector') $fn_options_array["langSelector_selected_{$opv->options_value}"] = 'selected';
-	
 	if($opv->options_key == 'showHeaderLangs') $fn_options_array["showHeaderLangs_{$opv->options_value}"] = 'selected';
 	if($opv->options_key == 'showHeaderLangsMob') $fn_options_array["showHeaderLangsMob_{$opv->options_value}"] = 'selected';
 	
@@ -100,55 +52,5 @@ if($fn_options_q) foreach($fn_options_q as $opk => $opv)
 $fn_xtemplate_parse['assign'][] = $fn_options_array;
 $fn_xtemplate_parse['parse'][] = '';
 
-// ----------------------------------------------------------------------------------
-
-//traducciones
-
-$fn_lang_q = $db->FetchAll("
-	SELECT * 
-	FROM `lang`
-");
-
-$fn_lang_data = array();
-
-if($fn_lang_q)
-{
-	//creamos keys
-	
-	foreach($fn_lang_q as $lk => $lv)
-	{
-		if(!in_array($lv->lang_key, $fn_lang_data))
-		{
-			$fn_lang_data[$lv->lang_key] = array();
-		}
-	}
-
-	//rellenamos
-	foreach($fn_lang_q as $llk => $llv)
-	{
-		$fn_lang_data[$llv->lang_key][$llv->lang_type] = $llv->lang_value;
-	}
-	
-	foreach($fn_lang_data as $ldk => $ldv)
-	{
-		$for_lang_rows = '';
-		
-		foreach($fn_lang as $gl)
-		{
-			$fn_xtemplate_parse['assign'][] = array(
-				'key' => $ldk,
-				'lang_k' => $gl,
-				'lang_v' => (isset($ldv[$gl])) ? $ldv[$gl] : '',
-			);
-			$fn_xtemplate_parse['parse'][] = "{$fn_page_args['stage_id']}.lang_table_value.lang_table_value_input";
-		}
-		
-		
-		$fn_xtemplate_parse['assign'][] = array(
-			'key' => $ldk,
-		);
-		$fn_xtemplate_parse['parse'][] = "{$fn_page_args['stage_id']}.lang_table_value";
-	}
-}
 
 ?>
